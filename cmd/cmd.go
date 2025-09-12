999da4f71e4b6b96c0fda99e16ffb3d16f5374a2
YelyzavetaV <velizhaninae@gmail.com>
2025-09-12 14:27:21 +0300

    feat: make commands execute concurrently

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,6 +7,7 @@ import (
 	"github.com/YelyzavetaV/country-fetcher/config"
 	"github.com/YelyzavetaV/country-fetcher/fetch"
 	"github.com/YelyzavetaV/country-fetcher/output"
+	"github.com/YelyzavetaV/country-fetcher/models"
 )
 
 var (
@@ -16,13 +17,13 @@ var (
 
 // Command-line arguments
 var (
-	name         string
-	region       string
-	code         string
-	fullText     bool
-	all          bool
-	n            int
-	filename     string
+	names    []string
+	codes    []string
+	regions  []string
+	fullText bool
+	all      bool
+	n        int
+	filename string
 )
 
 var RootCmd = &cobra.Command{
@@ -35,46 +36,35 @@ A CLI tool to fetch and process country and region data using RESTful Country AP
 }
 
 var fetchCountriesCmd = &cobra.Command{
-	Use:   "fetch",
+	Use:   "fetch-countries",
 	Short: "Get info about a country or countries",
 	Long:  `
-Get info about one or multiple countries by country name, region name, or country code.
+Get info about one or multiple countries by country name or country code.
 	`,
 	Run:   fetchCountries,
 }
 
-var processRegionCmd = &cobra.Command{
-	Use:   "region",
+var processRegionsCmd = &cobra.Command{
+	Use:   "fetch-regions",
 	Short: "Get stats of n or all countries in the region",
 	Long:  `
 Compute statistics for n or all countries in the region.
 	`,
-	Run:   processRegion,
+	Run:   processRegions,
 }
 
 func init() {
-	fetchCountriesCmd.Flags().StringVar(
-		&name, "name", "",
-			"Country name. Has precedence over --region and --code - " +
-			"providing a non-empty name leads to region and country code " +
-			"values being ignored.")
-	fetchCountriesCmd.Flags().StringVar(
-		&region, "region", "",
-			"Geographical region. Has precendence over --code - providing " +
-			"a non-empty region leads to the country code value being ignored.")
-	fetchCountriesCmd.Flags().StringVar(&code, "code", "", "Country code")
+	fetchCountriesCmd.Flags().StringSliceVar(
+		&names, "names", []string{},
+			"Country names. Has precedence over --codes - providing " +
+			"a non-empty --names leads to country codes being ignored.")
+	fetchCountriesCmd.Flags().StringSliceVar(
+		&codes, "codes", []string{}, "Country codes")
 	fetchCountriesCmd.Flags().BoolVar(
 		&fullText, "fulltext", false,
 			"Use full text name search. Only relevant when quering country by name.")
-	fetchCountriesCmd.Flags().BoolVar(
-		&all, "all", false,
-			"Fetch all matching countries (typically, in a region). " +
-			"Note that --all has precendence over --n, i.e., providing " +
-			"'--all true --n <value>' will lead to the value of n being ignored.")
 	fetchCountriesCmd.Flags().IntVar(
-		&n, "n", 1,
-			"Maximum number of countries to fetch. For all=false, " +
-			"a non-positive n is interpreted as all=true.")
+		&n, "n", 1, "Maximum number of countries to fetch.")
 	fetchCountriesCmd.Flags().StringVar(
 		&filename, "file", "",
 			"Name of a file the output is to be written to. If not " +
@@ -82,24 +72,26 @@ func init() {
 
 	RootCmd.AddCommand(fetchCountriesCmd)
 
-	processRegionCmd.Flags().StringVar(&name, "name", "", "Region name")
-	processRegionCmd.MarkFlagRequired("name")
+	processRegionsCmd.Flags().StringSliceVar(
+		&regions, "regions", []string{},
+			"Comma-separated list of region names (e.g., Europe,Asia,Africa)")
+	processRegionsCmd.MarkFlagRequired("regions")
 
-	processRegionCmd.Flags().BoolVar(
+	processRegionsCmd.Flags().BoolVar(
 		&all, "all", false,
 			"Fetch all countries in the region. Note that --all has " +
 			"precendence over --n, i.e., providing '--all true --n <value>' " +
 			"will lead to the value of n being ignored.")
-	processRegionCmd.Flags().IntVar(
+	processRegionsCmd.Flags().IntVar(
 		&n, "n", 10,
 			"Maximum number of countries to fetch. For all=false, " +
 			"a non-positive n is interpreted as all=true.")
-	processRegionCmd.Flags().StringVar(
+	processRegionsCmd.Flags().StringVar(
 		&filename, "file", "",
 			"Name of a file the output is to be written to. If not " +
 			"provided, JSON string is outputted to console.")
 
-	RootCmd.AddCommand(processRegionCmd)
+	RootCmd.AddCommand(processRegionsCmd)
 }
 
 func setup(cmd *cobra.Command, args []string) {
@@ -108,58 +100,94 @@ func setup(cmd *cobra.Command, args []string) {
 }
 
 func fetchCountries(cmd *cobra.Command, args []string) {
-	var query fetch.Query
-	if name != "" {
-		query = fetch.NameQuery{
-			Name:     name,
-			FullText:fullText,
+	var queries []fetch.Query
+
+	if len(names) != 0 {
+		for _, name := range names {
+			queries = append(
+				queries,
+				fetch.NameQuery{
+					Name:     name,
+					FullText: fullText,
+				},
+			)
 		}
-	} else if region != "" {
-		query = fetch.RegionQuery{
-			Region: region,
-		}
-	} else if code != "" {
-		query = fetch.CodeQuery{
-			Code: code,
+	} else if len(codes) != 0 {
+		for _, code := range codes {
+			queries = append(queries, fetch.CodeQuery{Code: code})
 		}
 	} else {
 		return
 	}
 
 	if all { n = -1 }
-	countries, err := client.FetchCountries(query, n)
-	if err != nil {
-		fmt.Printf("Failed to fetch data: %v", err)
-		return
+
+	ch := make(chan []models.Country, len(queries))
+
+	for _, query := range queries {
+		go func(countryQuery fetch.Query){
+			countries, err := client.FetchCountries(query, n)
+			if err != nil {
+				fmt.Printf("Failed to fetch data: %v", err)
+				ch <- nil
+				return
+			}
+			ch <- countries
+		}(query)
 	}
 
-	if err := output.ToJSON(
-		countries,
-		filename,
-		cfg.JSONPrefix,
-		cfg.JSONIndent,
-		cfg.JSONFilePermission,
-	); err != nil {
-		fmt.Printf("Failed to output data: %v", err)
+	for range queries {
+		countries := <-ch
+		if countries == nil {
+			fmt.Println("Countries fetch failed. Skipping...")
+			continue
+		}
+
+		if err := output.ToJSON(
+			countries,
+			filename,
+			cfg.JSONPrefix,
+			cfg.JSONIndent,
+			cfg.JSONFilePermission,
+		); err != nil {
+			fmt.Printf("Failed to output data: %v", err)
+		}
 	}
 }
 
-func processRegion(cmd *cobra.Command, args []string) {
+func processRegions(cmd *cobra.Command, args []string) {
 	if all { n = -1 }
 
-	region, err := client.ProcessRegion(name, n)
-	if err != nil {
-		fmt.Printf("Failed to get region data: %v", err)
-		return
+	ch := make(chan *models.Region, len(regions))
+
+	for _, r := range regions {
+		go func(name string) {
+			region, err := client.ProcessRegion(name, n)
+			if err != nil {
+				fmt.Printf(
+					"Failed to get data for region %s: %v", name, err)
+				ch <- nil
+				return
+			}
+			ch <- region
+		}(r)
 	}
 
-	if err := output.ToJSON(
-		region,
-		filename,
-		cfg.JSONPrefix,
-		cfg.JSONIndent,
-		cfg.JSONFilePermission,
-	); err != nil {
-		fmt.Printf("Failed to output data: %v", err)
+	for range regions {
+		region := <-ch
+		if region == nil {
+			fmt.Println("Region fetch failed. Skipping...")
+			continue
+		}
+
+		if err := output.ToJSON(
+			region,
+			filename,
+			cfg.JSONPrefix,
+			cfg.JSONIndent,
+			cfg.JSONFilePermission,
+		); err != nil {
+			fmt.Printf("Failed to output data: %v", err)
+		}
 	}
 }
